fdec042271980a3944bdf01bf03f7c9dfd1ac9d7
jilg <johannes.zilg@gmail.com>
2020-03-16 14:29:37 +0100

    correct name of head template

--- app/private/core/View.php
+++ app/private/core/View.php
@@ -6,7 +6,7 @@ class View
     {
         $this->styelUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/public/style.css';
 
-        require_once('./private/views/partials/header.php');
+        require_once('./private/views/partials/head.php');
         require_once('./private/views/' . $viewFile);
         require_once('./private/views/partials/footer.php');
     }
